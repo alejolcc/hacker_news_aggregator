fb16d6a93eff2df7ac7e6305531000799ed29203
alejo <alejo.rios@gmail.com>
2021-05-04 13:37:12 -0300

    add channel tests

--- test/hn_aggregator_web/channels/stories_channel_test.exs
+++ test/hn_aggregator_web/channels/stories_channel_test.exs
@@ -1,27 +1,41 @@
 defmodule HnAggregatorWeb.StoriesChannelTest do
-  use HnAggregatorWeb.ChannelCase
+  @moduledoc false
 
-  setup do
+  use HnAggregatorWeb.ChannelCase
+  alias HnAggregator.Repo
+
+  @stories [%{"id" => 1}, %{"id" => 2}, %{"id" => 3}]
+
+  setup_all do
+    :ok = Repo.push_stories(@stories)
+  end
+
+  defp join_and_subscribe do
     {:ok, _, socket} =
       HnAggregatorWeb.UserSocket
       |> socket("user_id", %{some: :assign})
-      |> subscribe_and_join(HnAggregatorWeb.StoriesChannel, "stories:lobby")
+      |> subscribe_and_join(HnAggregatorWeb.StoriesChannel, "stories:feed")
 
     %{socket: socket}
   end
 
-  test "ping replies with status ok", %{socket: socket} do
+  test "ping replies with status ok" do
+    %{socket: socket} = join_and_subscribe()
+
     ref = push(socket, "ping", %{"hello" => "there"})
     assert_reply ref, :ok, %{"hello" => "there"}
   end
 
-  test "shout broadcasts to stories:lobby", %{socket: socket} do
-    push(socket, "shout", %{"hello" => "all"})
-    assert_broadcast "shout", %{"hello" => "all"}
+  test "stories are pushed uppon connect the socket" do
+    %{socket: _socket} = join_and_subscribe()
+
+    assert_push "new stories", %{stories: @stories}
   end
 
-  test "broadcasts are pushed to the client", %{socket: socket} do
-    broadcast_from!(socket, "broadcast", %{"some" => "data"})
-    assert_push "broadcast", %{"some" => "data"}
+  test "stories are pushed when stories are updated" do
+    %{socket: _socket} = join_and_subscribe()
+
+    Phoenix.PubSub.broadcast(HnAggregator.PubSub, "stories", :new_stories)
+    assert_push "new stories", %{stories: @stories}
   end
 end
